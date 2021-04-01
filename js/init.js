964b30e4a8692aa89e9c41e8c3283bdac8a9be6b
chat42mhvm <81768894+chat42mhvm@users.noreply.github.com>
2021-04-01 17:54:41 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDN8Wv9rK3OuDETpum7zRPtqN9wbTfEsas",
+    authDomain: "autentifica41.firebaseapp.com",
+    projectId: "autentifica41",
+    storageBucket: "autentifica41.appspot.com",
+    messagingSenderId: "870800650160",
+    appId: "1:870800650160:web:85a4366e43080e24dbf075",
+    measurementId: "G-PWSWXDQ9DM"
+    });
